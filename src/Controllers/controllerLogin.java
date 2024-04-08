a3f03f1c50bdbcf90f920c5c04c70ebc1a20a79e
MathildePanier09 <125481345+MathildePanier09@users.noreply.github.com>
2024-04-08 19:49:50 +0200

    ventanas

---
+++ src/Controllers/controllerLogin.java
@@ -0,0 +1,25 @@
+package Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class controllerLogin {
+
+
+	@FXML
+	private TextField id;
+
+	@FXML
+	private TextField password;
+
+	@FXML
+	private Button entrarBttn;
+
+	@FXML
+	void loginAction(ActionEvent event) {
+
+	}
+
+}
