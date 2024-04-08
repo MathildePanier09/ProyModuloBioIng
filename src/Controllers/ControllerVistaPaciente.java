deaa5a848a979b7b1dc0dbd493b52cc43275374d
MathildePanier09 <125481345+MathildePanier09@users.noreply.github.com>
2024-04-08 20:29:38 +0200

    vista paciente

---
+++ src/Controllers/ControllerVistaPaciente.java
@@ -0,0 +1,47 @@
+package Controllers;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import users.Paciente;
+
+public class ControllerVistaPaciente {
+	
+
+    public ControllerVistaPaciente( Paciente paciente) {
+		super();
+		this.idPaciente = idPaciente;
+		this.visualizeBttn = visualizeBttn;
+		this.salirBttn = salirBttn;
+		this.paciente = paciente;
+	}
+
+	@FXML
+    private Label idPaciente;
+
+    @FXML
+    private Button visualizeBttn;
+
+    @FXML
+    private Button salirBttn;
+    
+    private Paciente paciente;
+
+    @FXML
+    void salirAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void visualizeAction(ActionEvent event) {
+
+    }
+    public void initialize(URL arg0, ResourceBundle arg1) {
+		idPaciente.setText(paciente.getId());
+		
+}
+}
+
